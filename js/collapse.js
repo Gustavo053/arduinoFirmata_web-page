4237b56ab3e0d9cc1ce370c83f2dfd43c258f53e
Gustavo Pereira <gu-gu-pf@hotmail.com>
2020-03-31 18:41:16 -0300

    inital commit

---
+++ js/collapse.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", event => {
+	let botao = document.getElementById("botao-menu");
+
+	let menu = document.getElementById("menu-mobile");
+
+	let botaoFechar = document.getElementById("botao-fechar");
+
+	function collapse() {
+		botao.addEventListener("click", () => {
+			menu.classList.add("collapse-menu");
+			botao.classList.add("display-none");
+			botaoFechar.classList.add("display-block");
+		});
+		botaoFechar.addEventListener("click", () => {
+			menu.classList.remove("collapse-menu");
+			botao.classList.remove("display-none");
+			botaoFechar.classList.remove("display-block");
+		});
+	}
+	collapse();
+});
+
+function getStarted() {
+	window.open("./installation.html", "Instalação", windowFeatures);
+}
